96e37ba899da10a18537ee126be49ed17ad20fde
jesszabala23 <jzabala23@gmail.com>
2020-08-10 17:43:06 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var globalVariable = "I am THEE Global Variable"
+function myFunction() {
+    var localVariable= "I am THEE Local Variable"
+    console.log(localVariable);
+    console.log(globalVariable);
+}
+myFunction()
+
+var firstName = "Jessica"
+var lastName = "Zabala"
+var favColor = "Purple"
+console.log(firstName);
+console.log(lastName);
+console.log(favColor);
